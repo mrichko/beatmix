e804979ab925ff000cf9e172db486d2571841aa1
Mike Richko <mrichko@icloud.com>
2018-10-02 14:24:35 -0500

    2bd code push

---
+++ public/js/script.js
@@ -0,0 +1,54 @@
+// Four Drum Arrays
+let kicks = Array(16).fill(false);
+let snares = Array(16).fill(false);
+let hiHats = Array(16).fill(false);
+let rideCymbals = Array(16).fill(false);
+
+// set variable of getDrumArrayByName to define the four options a user can select
+const getDrumArrayByName = (name) => {
+  switch (name){
+    case 'kick':
+    return 'kicks';
+
+    case 'snares':
+    return 'snares';
+
+    case 'hiHats':
+    return 'hiHats';
+
+    case 'rideCymbals':
+    return 'rideCymbals';
+
+    default:
+    return;
+  }
+};
+
+// set function that allows user to toggle a button off or on
+const toggleDrum = (drumArrayName, index) => {
+  const drums = getDrumArrayByName(drumArrayName);
+  if (!drums || index > 15 || index < 0) {
+    return;
+  }
+  drums[index] = !drums[index];
+};
+
+
+// sets function that clears all selections
+const clear = (drumArrayName) => {
+  const drums = getDrumArrayByName(drumArrayName);
+  if (drums) {
+    drums.fill(false);
+  }
+};
+
+// sets function that inverts the set choices
+const invert = (drumArrayName) => {
+  const drums = getDrumArrayByName(drumArrayName);
+  if (!drums) {
+    return
+  }
+  else (let selection = 0; if selection < drums.legenth; selection++){
+    drums[selection] = !drums[selection]
+  }
+};
